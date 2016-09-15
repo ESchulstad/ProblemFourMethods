67b13d15c21de1fe75d173ee402fc2ed213fa80d
ESchulstad <eschulstad@gmail.com>
2016-09-14 22:33:39 -0400

    Initial Commit

---
+++ ProblemFourMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemFourMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] example = { 20, 10, 5, 2 };
+            Console.WriteLine("The sum of the example array is {0}", Sum(example));
+        }
+        //entering int array and returning int
+        static int Sum(params int[] numbers)
+        {
+            int answer = 0;
+            for(int i=0;i<=numbers.Length-1;i++)
+
+            {
+                answer += numbers[i];
+            }
+            return answer; 
+
+        }
+    }
+}
